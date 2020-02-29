e4ff73fd106d64b91e41fb1af9dea1c9d0bdae38
simon3dv <gzfansiming@gmail.com>
2020-02-29 11:25:40 +0800

    cudnn_status_not_supported

--- tools/train_rcnn.py
+++ tools/train_rcnn.py
@@ -144,7 +144,7 @@ def create_scheduler(optimizer, total_steps, last_epoch):
 
 
 if __name__ == "__main__":
-    torch.backends.cudnn.enabled = False
+    torch.backends.cudnn.benchmark = True
     if args.cfg_file is not None:
         cfg_from_file(args.cfg_file)
     cfg.TAG = os.path.splitext(os.path.basename(args.cfg_file))[0]
