0ead0aa3c0d07f0ad1bf5b427e1b8c47309d7ca0
simon3dv <gzfansiming@gmail.com>
2020-03-28 10:45:22 +0800

    debug

--- lib/net/train_functions_da.py
+++ lib/net/train_functions_da.py
@@ -97,7 +97,7 @@ def model_joint_fn_decorator():
                 loss += da_rpn_loss*1.0 # DA_IMG_LOSS_WEIGHT
                 disp_dict['da_rpn_loss'] = da_rpn_loss.item()*1.0 # DA_IMG_LOSS_WEIGHT
             if cfg.RCNN.ENABLED and cfg.DA.DA_INS.ENABLED:
-                da_rcnn_loss = get_da_rcnn_loss(ret_dict['da_ins'], is_source_for_rois, tb_dict)
+                da_rcnn_loss = get_da_rcnn_loss(ret_dict['da_ins'], is_source_for_rois, data['is_source'], tb_dict)
                 loss += da_rcnn_loss * 1.0  # DA_INS_LOSS_WEIGHT
                 disp_dict['da_rcnn_loss'] = da_rcnn_loss.item() * 1.0
             if cfg.DA.DA_CST.ENABLED:
