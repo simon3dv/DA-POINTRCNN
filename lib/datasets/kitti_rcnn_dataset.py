50434c1568bb6b9422e981aba546114f77153a69
simon3dv <gzfansiming@gmail.com>
2020-04-22 01:07:15 +0800

    update

--- lib/datasets/kitti_rcnn_dataset.py
+++ lib/datasets/kitti_rcnn_dataset.py
@@ -309,7 +309,13 @@ class KittiRCNNDataset(KittiDataset):
             else:
                 choice = np.arange(0, len(pts_rect), dtype=np.int32)
                 if self.npoints > len(pts_rect):
-                    extra_choice = np.random.choice(choice, self.npoints - len(pts_rect), replace=False)
+                    m = int(self.npoints/len(pts_rect))
+                    if(m>1):
+                        choice_temp = choice.copy()
+                        while(m>1):
+                            choice = np.concatenate((choice, choice_temp), axis=0)
+                            m = m - 1
+                    extra_choice = np.random.choice(choice, (self.npoints - len(pts_rect)%len(pts_rect), replace=False))
                     choice = np.concatenate((choice, extra_choice), axis=0)
                 np.random.shuffle(choice)
 
